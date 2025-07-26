5ae0e876342f7ae68b9b6857c342ee4baf345ef7
id1509 <165214611+id1509@users.noreply.github.com>
2025-07-26 21:37:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/WizEdia_frontend',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
